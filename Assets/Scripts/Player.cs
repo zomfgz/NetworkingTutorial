44df83b0cfeba2727a1e0fc0768d43333c5d4155
zomfgz <speedgrapherorz@gmail.com>
2020-11-23 16:44:35 -0800

    Hello world - multiplayer edition

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, 0, moveVertical * 0.1f);
+            transform.position = transform.position + movement;
+
+        }
+    }
+
+    private void Update()
+    {
+        {
+            HandleMovement();
+        }
+    }
+}
